a598ab86ced85de6b682b7c5b27b233d000d0b79
Sanna Volanen <sanna.i.volanen@utu.fi>
2019-12-30 21:17:10 +0200

    fiddling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/sainvo"
           target="_blank"
           rel="noopener noreferrer"
         >
---
+++ src/components/Professions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Professions = () =>{
+     return(
+          <div className="professions">
+               <ul class="flex-container">
+                    <li class="flex-item">Translator</li>
+                    <li class="flex-item">Translator</li>
+                    <li class="flex-item">Translator</li>
+               </ul>
+          </div>
+     )
+}
